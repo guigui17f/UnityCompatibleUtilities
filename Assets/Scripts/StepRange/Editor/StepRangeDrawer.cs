af4114cfa433ce2899b0d1082edbc56e7b4b5a49
guigui17f <guigui17f0@gmail.com>
2022-10-24 20:28:42 +0800

    add step range attribute

---
+++ Assets/Scripts/StepRange/Editor/StepRangeDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(StepRangeAttribute))]
+public class StepRangeDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        StepRangeAttribute stepRange = attribute as StepRangeAttribute;
+        if (property.propertyType == SerializedPropertyType.Float)
+        {
+            int multiplier = Convert.ToInt32(property.floatValue / stepRange.Step);
+            property.floatValue = stepRange.Step * multiplier;
+            EditorGUI.Slider(position, property, stepRange.Min, stepRange.Max, label);
+        }
+        else if (property.propertyType == SerializedPropertyType.Integer)
+        {
+            EditorGUI.IntSlider(position, property, (int)stepRange.Min, (int)stepRange.Max, label);
+        }
+        else
+        {
+            EditorGUI.LabelField(position, label.text, "use range with float or int.");
+        }
+    }
+}
---
+++ Assets/Scripts/StepRange/StepRangeAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class StepRangeAttribute : PropertyAttribute
+{
+    public readonly float Min;
+    public readonly float Max;
+    public readonly float Step;
+
+    public StepRangeAttribute(float min, float max, float step)
+    {
+        Min = min;
+        Max = max;
+        Step = step;
+    }
+}
