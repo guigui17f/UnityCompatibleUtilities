58c3bb5844a15f2f1bc6ec8155c696723c93e2b5
guigui17f <guigui17f0@gmail.com>
2022-10-24 22:56:18 +0800

    add singleton base class

---
+++ Assets/Scripts/Models/SingletonBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GUIGUI17F
+{
+    public class SingletonBase<T> : IDisposable where T : class, new()
+    {
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new T();
+                }
+                return _instance;
+            }
+        }
+
+        protected static T _instance;
+
+        protected SingletonBase()
+        {
+        }
+
+        public virtual void Dispose()
+        {
+            _instance = null;
+        }
+    }
+}
