2cb07125e9fbd75281f3bf30d4f11fe4682e70c6
guigui17f <guigui17f0@gmail.com>
2022-10-24 22:55:17 +0800

    refactor file structures

---
+++ Assets/Scripts/Utilities/StepRange/StepRangeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace GUIGUI17F
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class StepRangeAttribute : PropertyAttribute
+    {
+        public readonly float Min;
+        public readonly float Max;
+        public readonly float Step;
+
+        public StepRangeAttribute(float min, float max, float step)
+        {
+            Min = min;
+            Max = max;
+            Step = step;
+        }
+    }
+}
