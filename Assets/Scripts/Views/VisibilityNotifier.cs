b1f78dab795ded47305f638948b65ad96fa759a8
guigui17f <guigui17f0@gmail.com>
2022-10-26 23:46:16 +0800

    add a renderer visibility notifier

---
+++ Assets/Scripts/Views/VisibilityNotifier.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+
+namespace GUIGUI17F
+{
+    /// <summary>
+    /// renderer visibility notifier
+    /// </summary>
+    [RequireComponent(typeof(Renderer))]
+    public class VisibilityNotifier : MonoBehaviour
+    {
+        /// <summary>
+        /// parameter: is visible
+        /// </summary>
+        public event Action<bool> OnVisibilityChanged;
+
+        public void OnBecameVisible()
+        {
+            OnVisibilityChanged?.Invoke(true);
+        }
+        
+        public void OnBecameInvisible()
+        {
+            OnVisibilityChanged?.Invoke(false);
+        }
+    }
+}
