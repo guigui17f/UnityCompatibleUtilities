f0be6f983ced9977f267a287a5a047fb348f3c64
guigui17f <guigui17f0@gmail.com>
2022-10-26 23:34:51 +0800

    add CameraMatrixController

--- Assets/Scripts/Views/BackgroundAdjuster.cs
+++ Assets/Scripts/Views/BackgroundAdjuster.cs
@@ -1,59 +1,62 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// adjust a sprite to full screen
-/// </summary>
-public class BackgroundAdjuster : MonoBehaviour
+namespace GUIGUI17F
 {
-    public Camera AnchorCamera;
-    public SpriteRenderer TargetRenderer;
-
-    private int _currentWidth;
-    private int _currentHeight;
-
-    private void Start()
+    /// <summary>
+    /// adjust a sprite to full screen
+    /// </summary>
+    public class BackgroundAdjuster : MonoBehaviour
     {
-        if (null == AnchorCamera)
-        {
-            AnchorCamera = Camera.main;
-        }
-        if (null == TargetRenderer)
-        {
-            TargetRenderer = GetComponent<SpriteRenderer>();
-        }
-        AdjustBackground();
-    }
+        public Camera AnchorCamera;
+        public SpriteRenderer TargetRenderer;
 
-    private void AdjustBackground()
-    {
-        _currentWidth = Screen.width;
-        _currentHeight = Screen.height;
-        Transform cameraTransform = AnchorCamera.transform;
-        Transform rendererTransform = TargetRenderer.transform;
-        Vector3 originSize = TargetRenderer.bounds.size;
-        float originRatio = originSize.x / originSize.y;
-        Vector3 originScale = rendererTransform.localScale;
-        
-        float cameraDistance = Vector3.Dot(rendererTransform.position - cameraTransform.position, cameraTransform.forward);
-        Vector3 newSize = AnchorCamera.ScreenToWorldPoint(new Vector3(_currentWidth, _currentHeight, cameraDistance)) * 2;
-        float newRatio = newSize.x / newSize.y;
-        if (originRatio > newRatio)
-        {
-            float scale = newSize.y / originSize.y;
-            rendererTransform.localScale = new Vector3(originScale.x * scale, originScale.y * scale, originScale.z);
-        }
-        else
-        {
-            float scale = newSize.x / originSize.x;
-            rendererTransform.localScale = new Vector3(originScale.x * scale, originScale.y * scale, originScale.z);
-        }
-    }
+        private int _currentWidth;
+        private int _currentHeight;
 
-    private void Update()
-    {
-        if (Screen.width != _currentWidth || Screen.height != _currentHeight)
+        private void Start()
         {
+            if (null == AnchorCamera)
+            {
+                AnchorCamera = Camera.main;
+            }
+            if (null == TargetRenderer)
+            {
+                TargetRenderer = GetComponent<SpriteRenderer>();
+            }
             AdjustBackground();
+        }
+
+        private void AdjustBackground()
+        {
+            _currentWidth = Screen.width;
+            _currentHeight = Screen.height;
+            Transform cameraTransform = AnchorCamera.transform;
+            Transform rendererTransform = TargetRenderer.transform;
+            Vector3 originSize = TargetRenderer.bounds.size;
+            float originRatio = originSize.x / originSize.y;
+            Vector3 originScale = rendererTransform.localScale;
+
+            float cameraDistance = Vector3.Dot(rendererTransform.position - cameraTransform.position, cameraTransform.forward);
+            Vector3 newSize = AnchorCamera.ScreenToWorldPoint(new Vector3(_currentWidth, _currentHeight, cameraDistance)) * 2;
+            float newRatio = newSize.x / newSize.y;
+            if (originRatio > newRatio)
+            {
+                float scale = newSize.y / originSize.y;
+                rendererTransform.localScale = new Vector3(originScale.x * scale, originScale.y * scale, originScale.z);
+            }
+            else
+            {
+                float scale = newSize.x / originSize.x;
+                rendererTransform.localScale = new Vector3(originScale.x * scale, originScale.y * scale, originScale.z);
+            }
+        }
+
+        private void Update()
+        {
+            if (Screen.width != _currentWidth || Screen.height != _currentHeight)
+            {
+                AdjustBackground();
+            }
         }
     }
 }
---
+++ Assets/Scripts/Views/CameraMatrixController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace GUIGUI17F
+{
+    public class CameraMatrixController : MonoBehaviour
+    {
+        public float RotateAngle = 0;
+        public float Scale = 1;
+        public Vector2 Shearing = Vector2.zero;
+        public Vector2 Translation = Vector2.zero;
+        public Camera TargetCamera;
+
+        private Matrix4x4 _matrixRotate = Matrix4x4.identity;
+        private Matrix4x4 _matrixScale = Matrix4x4.identity;
+        private Matrix4x4 _matrixXShearing = Matrix4x4.identity;
+        private Matrix4x4 _matrixYShearing = Matrix4x4.identity;
+        private Matrix4x4 _matrixTranslation = Matrix4x4.identity;
+        private Matrix4x4 _originMatrixProjection;
+
+        private void Start()
+        {
+            if (TargetCamera == null)
+            {
+                TargetCamera = Camera.main;
+            }
+            _originMatrixProjection = TargetCamera.projectionMatrix;
+        }
+
+        private void Update()
+        {
+            _matrixScale[0, 0] = Scale;
+            _matrixScale[1, 1] = Scale;
+
+            _matrixRotate[0, 0] = Mathf.Cos(RotateAngle * Mathf.Deg2Rad);
+            _matrixRotate[0, 1] = -Mathf.Sin(RotateAngle * Mathf.Deg2Rad);
+            _matrixRotate[1, 0] = Mathf.Sin(RotateAngle * Mathf.Deg2Rad);
+            _matrixRotate[1, 1] = Mathf.Cos(RotateAngle * Mathf.Deg2Rad);
+
+            _matrixXShearing[0, 1] = Shearing.x;
+            _matrixYShearing[1, 0] = Shearing.y;
+
+            _matrixTranslation[0, 2] = Translation.x;
+            _matrixTranslation[1, 2] = Translation.y;
+
+            TargetCamera.projectionMatrix = _originMatrixProjection * _matrixScale * _matrixRotate * _matrixXShearing * _matrixYShearing * _matrixTranslation;
+        }
+    }
+}
