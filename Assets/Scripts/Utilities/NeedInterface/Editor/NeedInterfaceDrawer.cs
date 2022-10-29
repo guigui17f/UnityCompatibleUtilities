2ae62954de753d6ccc10607dd4fdfaa62884ae13
guigui17f <guigui17f0@gmail.com>
2022-10-29 21:17:44 +0800

    add NeedInterface attribute

---
+++ Assets/Scripts/Utilities/NeedInterface/Editor/NeedInterfaceDrawer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace GUIGUI17F
+{
+    [CustomPropertyDrawer(typeof(NeedInterfaceAttribute))]
+    public class NeedInterfaceDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.ObjectReference)
+            {
+                EditorGUI.LabelField(position, label.text, "NeedInterface attribute can only be used with MonoBehaviours!");
+                return;
+            }
+            NeedInterfaceAttribute interfaceAttribute = attribute as NeedInterfaceAttribute;
+            EditorGUI.BeginProperty(position, label, property);
+            property.objectReferenceValue = EditorGUI.ObjectField(position, label, property.objectReferenceValue, interfaceAttribute.InterfaceType, true);
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/NeedInterface/NeedInterfaceAttribute.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+namespace GUIGUI17F
+{
+    /// <summary>
+    /// use this attribute on a MonoBehaviour field to claim this MonoBehaviour must implement a certain interface
+    /// </summary>
+    public class NeedInterfaceAttribute : PropertyAttribute
+    {
+        public readonly Type InterfaceType;
+
+        public NeedInterfaceAttribute(Type interfaceType)
+        {
+            InterfaceType = interfaceType;
+        }
+    }
+}
