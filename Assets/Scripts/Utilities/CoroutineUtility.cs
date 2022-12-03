59284936e9a857efebaadfc40a9483f115936603
guigui17f <guigui17f0@gmail.com>
2022-12-03 17:49:59 +0800

    add coroutine utility

---
+++ Assets/Scripts/Utilities/CoroutineUtility.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GUIGUI17F
+{
+    public class CoroutineUtility : MonoBehaviour
+    {
+        public static CoroutineUtility Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GameObject("CoroutineUtility").AddComponent<CoroutineUtility>();
+                }
+                return _instance;
+            }
+        }
+        
+        private static CoroutineUtility _instance;
+        
+        private void Awake()
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        
+        public static Coroutine StartMonoCoroutine(IEnumerator enumerator)
+        {
+            return Instance.StartCoroutine(enumerator);
+        }
+
+        public static void StopMonoCoroutine(Coroutine coroutine)
+        {
+            Instance.StopCoroutine(coroutine);
+        }
+
+        public static void StopAllMonoCoroutines()
+        {
+            Instance.StopAllCoroutines();
+        }
+
+        private void OnDestroy()
+        {
+            _instance = null;
+        }
+    }
+}
